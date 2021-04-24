83ea885c947b9549e3df36d5b808085cf6b0b051
Antonio Carlos <antonie.dev@gmail.com>
2021-04-23 21:16:51 -0300

    add audio

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,6 +3,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -13,7 +14,7 @@ type Episode = {
   title: string;
   members: string;
   thumbnail: string;
-  duration: string;
+  duration: number;
   durationAsString: string;
   url: string;
   publishedAt: string;
@@ -61,8 +62,23 @@ export default function Episode({ episode }: EpisodesProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id,
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
